ac9d804f95966db06349415aa3754bbc58c04ea0
Lucas Castro <lucas.castro@corp.globo.com>
2016-01-12 11:35:56 -0200

    add angular from Inspinia/Angular_Seed

---
+++ app/static/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+(function () {
+    var app = angular.module('inspinia', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+    ])
+    app.config(['$interpolateProvider', function($interpolateProvider) {
+  		$interpolateProvider.startSymbol('{[');
+  		$interpolateProvider.endSymbol(']}');
+	}]);
+})();
+
