44c9865d6b9302c7290cd96f44169d8f811ab1e8
Lucas Castro <lucas.castro@corp.globo.com>
2016-01-12 18:37:36 -0200

    list project vm dethails and list vms

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -13,9 +13,27 @@ function MainCtrl($http) {
     this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
 
 	this.countVmsByProject = function() {
+		console.log('load vms count ...')
     	$http.get('/api/v1/lab/project/?account_name=timeevolucaoinfra&domain_id=28f40084-2aed-11e5-8fce-76b2dd27c282')
     	.success(function(response) {
-    		mainCtrl.countVmsByProject = response
+    		mainCtrl.countVmsByProject = response;
+    	})
+    }
+
+    this.loadProject = function(projectId) {
+    	console.log('load project... ');
+    	console.log(projectId);
+    	$http.get('/api/v1/lab/vm_count/?project_id=' + projectId)
+    	.success(function(response){
+    		mainCtrl.project = response;
+    	})
+    }
+    this.listVirtualMachines = function(projectId){
+    	console.log('load vms... ');
+    	console.log(projectId);
+    	$http.get('/api/v1/lab/virtual_machine/?project_id=' + projectId)
+    	.success(function(response){
+    		mainCtrl.instances = response.virtual_machines;
     	})
     }
 };
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
@@ -8,5 +8,10 @@ def index():
 
 
 @app.route('/instances')
-def menu1():
-    return render_template('instances.html')
+def instances():
+    return render_template('instances/index.html')
+
+@app.route('/instances/<project_name>')
+def instances_project(project_name):
+	project_id = request.args.get('project_id', '')
+	return render_template('instances/project.html', project_name=project_name, project_id=project_id)
