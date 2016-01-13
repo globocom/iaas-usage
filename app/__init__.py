b75f54b80195bb2e8daf26b2a030fa910de6814c
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2016-01-13 15:46:45 -0200

    Modules refactory

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 # Define the WSGI application object
 from flask_restful import Api
-from app.resources.projects.resource import ProjectResource
-from app.resources.users.resource import UserResource
-from app.resources.virtualmachines.resource import VirtualMachineResource
-from app.resources.vm_count.resource import VmCountResource
+from app.projects.resource import ProjectResource
+from app.users.resource import UserResource
+from app.virtualmachines.resource import VirtualMachineResource
+from app.vm_count.resource import VmCountResource
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +14,7 @@ logger = app.logger
 # Configurations placed in config.py in root directory
 app.config.from_object('config')
 
-#URL Mappings
+# URL Mappings
 api.add_resource(ProjectResource, '/api/v1/<region>/project/', endpoint='project')
 api.add_resource(UserResource, '/api/v1/<region>/user/<string:username>', endpoint='user')
 api.add_resource(VirtualMachineResource, '/api/v1/<region>/virtual_machine/', endpoint='virtual_machine')
