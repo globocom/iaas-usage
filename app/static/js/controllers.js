e64f47381ac52e1e59fc82c45e7f11b653488830
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2016-01-18 14:52:15 -0200

    ProjectCtrl created Refactory breadcrumb

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,35 +1,30 @@
 var UserCtrl = function($scope, $http, $stateParams, $state, $q) {
     userCtrl = this;
-    
+    this.user = null;
+
     userCtrl.loadUser = function() {
         console.log('Loading user')
         $http.get('/api/v1/lab/current_user/')
         .success(function(response){
-            $scope.user = response[0]
+            userCtrl.user = response[0]
             $scope.$broadcast('userLoaded', $scope.user);
         })
     }
 };
 
 function InstanceCtrl($scope, $http, $stateParams, $state){
-    var instanceCtrl = this
+    instanceCtrl = this
     this.title = 'Instances';
-
-    instanceCtrl.project = {name: $stateParams.projectName, id: $stateParams.projectId}
-
-    instanceCtrl.listProjects = function(event, user) {
-        console.log('Loading projects')
-        $http.get('/api/v1/lab/project/?account_name='+ user.account_name +'&domain_id=' + user.domain_id)
-        .success(function(response) {
-            $scope.projects = response;
-        });
-    }
+    this.projectName = '';
+    this.vmCount = []
+    this.instances = []
 
     instanceCtrl.getVmCount = function() {
+        instanceCtrl.projectName = $stateParams.projectName
         console.log('Loading vm count')
         $http.get('/api/v1/lab/vm_count/?project_id=' + $stateParams.projectId)
         .success(function(response){
-            $scope.vmCount = response;
+            instanceCtrl.vmCount = response;
         })
     }
 
@@ -37,8 +32,27 @@ function InstanceCtrl($scope, $http, $stateParams, $state){
         console.log('Loading virtual machines')
         $http.get('/api/v1/lab/virtual_machine/?project_id=' + $stateParams.projectId)
         .success(function(response){
-            $scope.instances = response.virtual_machines;
+            instanceCtrl.instances = response.virtual_machines;
         })
+    }
+
+    $scope.$on('userLoaded', this.listProjects)
+}
+
+function ProjectCtrl($scope, $http, $stateParams){
+    projectCtrl = this
+    this.projects
+    //projectCtrl.project = {name: $stateParams.projectName, id: $stateParams.projectId}
+
+    projectCtrl.listProjects = function(event, user) {
+        user = user || userCtrl.user
+        if(angular.isUndefined(this.projects) && user != null){
+            console.log('Loading projects')
+            $http.get('/api/v1/lab/project/?account_name='+ user.account_name +'&domain_id=' + user.domain_id)
+            .success(function(response) {
+                projectCtrl.projects = response;
+            });
+        }
     }
 
     $scope.$on('userLoaded', this.listProjects)
@@ -47,4 +61,5 @@ function InstanceCtrl($scope, $http, $stateParams, $state){
 angular
     .module('iaasusage')
     .controller('UserCtrl', UserCtrl)
+    .controller('ProjectCtrl', ProjectCtrl)
     .controller('InstanceCtrl', InstanceCtrl)
