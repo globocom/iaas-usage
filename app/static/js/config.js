71abd51697d2e798699d00a0af0acb9d58c4bd47
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2016-02-24 16:16:45 -0300

    Fiz size column ordering

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -123,7 +123,7 @@ function byteFilter() {
         if (typeof precision === 'undefined') precision = 1;
         var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
             number = Math.floor(Math.log(bytes) / Math.log(1024));
-        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision);
     }
 }
 
