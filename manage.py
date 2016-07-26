09e2dae55839f653351e353fac954bbd71af44e8
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2016-07-26 13:28:06 -0300

    Add script for runing app with gunicorn

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
-from flask_script import Manager
+from flask_script import Manager, Command, Option
 from app import app
 
 manager = Manager(app)
-
 
 @manager.command
 def create_db():
@@ -24,6 +23,29 @@ def destroy_db():
 def runserver():
     """"Runs the application on the default port (8080)"""
     app.run(port=8080)
+
+
+class GunicornServer(Command):
+    """Run the app within Gunicorn"""
+
+    def get_options(self):
+        from gunicorn.config import make_settings
+
+        settings = make_settings()
+        options = (
+            Option(*klass.cli, action=klass.action)
+            for setting, klass in settings.iteritems() if klass.cli
+        )
+        return options
+
+    def run(self, *args, **kwargs):
+        from gunicorn.app.wsgiapp import WSGIApplication
+
+        app = WSGIApplication()
+        app.app_uri = 'manage:app'
+        return app.run()
+
+manager.add_command("gunicorn", GunicornServer())
 
 if __name__ == "__main__":
     manager.run()
